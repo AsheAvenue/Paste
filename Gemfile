7e50103ec1c7fe8b001516e5c071fdfca49fdc88
Tim Boisvert <tboisvert@asheavenue.com>
2015-02-06 14:59:09 -0500

    Updated a few more styles, and updated env example

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'rethinkdb'
 gem 'thin'
 gem 'dotenv'
 gem 'sequel'
--- paste.rb
+++ paste.rb
@@ -1,7 +1,6 @@
 # encoding: UTF-8
 
 require 'sinatra'
-require 'rethinkdb'
 require 'net/http'
 require 'dotenv'
 require 'sequel'
@@ -11,10 +10,6 @@ Dotenv.load
 
 # Set up the config values
 CONFIG = {
-  :host   => ENV['RDB_HOST'], 
-  :port   => ENV['RDB_PORT'],
-  :db     => ENV['RDB_DB'],
-  :table  => ENV['RDB_TABLE'],
   :user   => ENV['PASTE_USER'],
   :pass   => ENV['PASTE_PASS'],
   :mysql_host   => ENV['MYSQL_HOST'],
@@ -24,40 +19,9 @@ CONFIG = {
   :mysql_table  => ENV['MYSQL_TABLE']
 }
 
-# Create the Rethink object
-r = RethinkDB::RQL.new
-
 # Some basic ht auth
 use Rack::Auth::Basic, "Authorization Required" do |username, password|
   username == CONFIG[:user] and password == CONFIG[:pass]
-end
-
-# When the app starts, ensure the database is set up
-configure do
-  set :db, CONFIG[:db]
-  connection = RethinkDB::Connection.new(:host => CONFIG[:host], :port => CONFIG[:port])
-  begin
-    r.db_create(CONFIG[:db]).run(connection)
-    r.db(CONFIG[:db]).table_create(CONFIG[:table]).run(connection)
-  rescue RethinkDB::RqlRuntimeError => err
-    puts "Database `#{CONFIG[:db]}` and table `#{CONFIG[:table]}` already exist."
-  ensure
-    connection.close
-  end
-end
-
-# Always each request by opening a connection to Rethink
-before do
-  begin
-    @rdb_connection = r.connect(:host => CONFIG[:host], :port => CONFIG[:port], :db => CONFIG[:db])
-  rescue Exception => err
-    halt 501, 'Cannot connect to database'
-  end
-end
-
-# Always finish each request by closing the Rethink connection
-after do
-  @rdb_connection.close if @rdb_connection
 end
 
 # Show the 'new paste' screen 
@@ -65,33 +29,6 @@ get '/' do
   id = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(5).join
   redirect "/#{id}"
 end
-
-# get "/migrate" do
-#
-#   db = Sequel.connect("mysql://#{CONFIG[:mysql_user]}:#{CONFIG[:mysql_pass]}@#{CONFIG[:mysql_host]}/#{CONFIG[:mysql_db]}")
-#
-#   # Delete the old pastes from the db
-#   puts "Deleting previous entries"
-#   db.run("DELETE FROM pastes")
-#
-#   # Get all the pastes from Rethink
-#   pastes = r.table(CONFIG[:table]).run(@rdb_connection).to_a
-#   pastes.each do |paste|
-#     puts "Migrating paste: #{paste['id']}"
-#     body = paste['body'].gsub("'", %q(\\\'))
-#     formatted_body = paste['formatted_body'].gsub("'", %q(\\\'))
-#     begin
-#       if paste['id'] != ''
-#         db.run("INSERT INTO pastes (id, body, formatted_body, created_at, lang) VALUES('#{paste['id']}', '#{body}', '#{formatted_body}', #{paste['created_at']}, '#{paste['lang']}')")
-#       end
-#     rescue
-#     end
-#   end
-#
-#   db.disconnect
-#
-#   'Done'
-# end
 
 # Show a paste or the edit form
 get '/:id' do
